4aac7681d9efc779b1e5fbd1336205927b0003b1
taymourmansi <37144922+taymourmansi@users.noreply.github.com>
2018-11-27 22:28:21 -0500

    TicTacToe
    (GUI Version)

---
+++ TicTacToe/src/Client.java
@@ -0,0 +1,152 @@
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Client {
+
+	private JFrame frame = new JFrame("Tic Tac Toe");
+	private JLabel msgLbl = new JLabel("");
+	private ImageIcon img;
+	private ImageIcon oppImg;
+
+	private Box[] gameBoard = new Box[9];
+	private Box currSquare;
+
+	private static int PORT = 8901;
+	private Socket socket;
+	private BufferedReader input;
+	private PrintWriter output;
+	private boolean win = false;
+	private boolean lose = false;
+	private boolean tie = false;
+
+
+	public Client(String address) throws Exception {
+
+		socket = new Socket(address, PORT);
+		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		output = new PrintWriter(socket.getOutputStream(), true);
+
+		msgLbl.setBackground(Color.white);
+		frame.getContentPane().add(msgLbl, "South");
+
+		JPanel boardPanel = new JPanel();
+		boardPanel.setBackground(Color.black);
+		boardPanel.setLayout(new GridLayout(3, 3, 2, 2));
+		for (int i = 0; i < gameBoard.length; i++) {
+			final int j = i;
+			gameBoard[i] = new Box();
+			gameBoard[i].addMouseListener(new MouseAdapter() {
+				public void mousePressed(MouseEvent e) {
+					currSquare = gameBoard[j];
+					output.println("MOVE " + j);
+				}
+			});
+			boardPanel.add(gameBoard[i]);
+		}
+		frame.getContentPane().add(boardPanel, "Center");
+	}
+
+	public void play() throws Exception {
+		String response;
+		try {
+			response = input.readLine();
+			if (response.startsWith("WELCOME")) {
+				char shape = response.charAt(8);
+				img = new ImageIcon(shape == 'X' ? "x.png" : "o.png");
+				oppImg = new ImageIcon(shape == 'X' ? "o.png" : "x.png");
+				frame.setTitle("Player " + shape);
+			}
+			while (true) {
+				response = input.readLine();
+				if (response.startsWith("VALID_MOVE")) {
+					msgLbl.setText("Waiting for Opponent");
+					currSquare.setIcon(img);
+					currSquare.repaint();
+				} else if (response.startsWith("OPPONENT_MOVED")) {
+					int space = Integer.parseInt(response.substring(15));
+					gameBoard[space].setIcon(oppImg);
+					gameBoard[space].repaint();
+					msgLbl.setText("It's your turn");
+				} else if (response.startsWith("VICTORY")) {
+					win = true;
+					msgLbl.setText("Winner!");
+					break;
+				} else if (response.startsWith("DEFEAT")) {
+					lose = true;
+					msgLbl.setText("LOSER!");
+					break;
+				} else if (response.startsWith("TIE")) {
+					tie = true;
+					msgLbl.setText("Tie");
+					break;
+				} else if (response.startsWith("MESSAGE")) {
+					msgLbl.setText(response.substring(8));
+				}
+			}
+			output.println("QUIT");
+		} finally {
+			socket.close();
+		}
+	}
+
+	private boolean rematch() {
+		if (win == true) {
+			int response = JOptionPane.showConfirmDialog(frame, "Play Again?", "You Win!", JOptionPane.YES_NO_OPTION);
+			frame.dispose();
+			return response == JOptionPane.YES_OPTION;
+
+		} else if (lose == true) {
+			int response = JOptionPane.showConfirmDialog(frame, "Play Again?", "You Lose!", JOptionPane.YES_NO_OPTION);
+			frame.dispose();
+			return response == JOptionPane.YES_OPTION;
+
+		} else {
+			int response = JOptionPane.showConfirmDialog(frame, "Play Again?", "You Tied!", JOptionPane.YES_NO_OPTION);
+			frame.dispose();
+			return response == JOptionPane.YES_OPTION;
+
+		}
+
+	}
+
+	static class Box extends JPanel {
+		JLabel label = new JLabel((Icon) null);
+
+		public Box() {
+			setBackground(Color.white);
+			add(label);
+		}
+
+		public void setIcon(Icon img) {
+			label.setIcon(img);
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		boolean control = true;
+		while (control) {
+			String address = (args.length == 0) ? "localhost" : args[1];
+			Client client = new Client(address);
+			client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			client.frame.setResizable(false);
+			client.frame.setVisible(true);
+			client.frame.setSize(500, 500);
+			client.play();
+			if (!client.rematch()) {
+				break;
+			}
+		}
+	}
+}
---
+++ TicTacToe/src/Server.java
@@ -0,0 +1,127 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+	public static void main(String[] args) throws Exception {
+		ServerSocket listener = new ServerSocket(8901);
+		System.out.println("Server is Running");
+		try {
+			while (true) {
+				Game game = new Game();
+				Game.Player playerOne = game.new Player(listener.accept(), 'X');
+				Game.Player playerTwo = game.new Player(listener.accept(), 'O');
+				playerOne.setOpp(playerTwo);
+				playerTwo.setOpp(playerOne);
+				game.currentPlayer = playerOne;
+				playerOne.start();
+				playerTwo.start();
+			}
+		} finally {
+			listener.close();
+		}
+	}
+}
+
+class Game {
+	private Player[] board = { null, null, null, null, null, null, null, null, null };
+	Player currentPlayer;
+
+	public boolean winner() {
+		return (board[0] != null && board[0] == board[1] && board[0] == board[2])
+				|| (board[3] != null && board[3] == board[4] && board[3] == board[5])
+				|| (board[6] != null && board[6] == board[7] && board[6] == board[8])
+				|| (board[0] != null && board[0] == board[3] && board[0] == board[6])
+				|| (board[1] != null && board[1] == board[4] && board[1] == board[7])
+				|| (board[2] != null && board[2] == board[5] && board[2] == board[8])
+				|| (board[0] != null && board[0] == board[4] && board[0] == board[8])
+				|| (board[2] != null && board[2] == board[4] && board[2] == board[6]);
+	}
+
+	public boolean fullBoard() {
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public synchronized boolean validMove(int position, Player player) {
+		if (player == currentPlayer && board[position] == null) {
+			board[position] = currentPlayer;
+			currentPlayer = currentPlayer.opp;
+			currentPlayer.otherPlayerMoved(position);
+			return true;
+		}
+		return false;
+	}
+
+	class Player extends Thread {
+		char shape;
+		Player opp;
+		Socket socket;
+		BufferedReader input;
+		PrintWriter output;
+
+		public Player(Socket socket, char shape) {
+			this.socket = socket;
+			this.shape = shape;
+			try {
+				input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				output = new PrintWriter(socket.getOutputStream(), true);
+				output.println("WELCOME " + shape);
+				output.println("MESSAGE Waiting for opponent to connect");
+			} catch (IOException e) {
+				System.out.println("Player left: " + e);
+			}
+		}
+
+		public void setOpp(Player opp) {
+			this.opp = opp;
+		}
+
+
+		public void otherPlayerMoved(int position) {
+			output.println("OPPONENT_MOVED " + position);
+			output.println(winner() ? "DEFEAT" : fullBoard() ? "TIE" : "");
+		}
+
+		public void run() {
+			try {
+				output.println("MESSAGE All players connected");
+
+				if (shape == 'X') {
+					output.println("MESSAGE Your move");
+				}
+
+				while (true) {
+					String str = input.readLine();
+					if (str.startsWith("MOVE")) {
+						int location = Integer.parseInt(str.substring(5));
+						if (validMove(location, this)) {
+							output.println("VALID_MOVE");
+							output.println(winner() ? "VICTORY" : fullBoard() ? "TIE" : "");
+						} else {
+							output.println("MESSAGE ?");
+						}
+					} else if (str.startsWith("QUIT")) {
+						return;
+					}
+				}
+			} catch (IOException e) {
+				System.out.println("Player left: " + e);
+			} finally {
+				try {
+					socket.close();
+				} catch (IOException e) {
+				}
+			}
+		}
+	}
+}
+
